487e42af94466300a200975c8f557fbb6ec756b3
Rajesh <rajesh.vadlamudi999@gmail.com>
2021-05-16 15:34:22 +0530

    update the changes for homepage method

--- src/test/java/com/shopping/stepdefinitions/MyAccountHomePageStepdefinition.java
+++ src/test/java/com/shopping/stepdefinitions/MyAccountHomePageStepdefinition.java
@@ -40,20 +40,12 @@ public class MyAccountHomePageStepdefinition {
 
 	@When("Account section is available")
 	public void account_section_is_available() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		
 	}
 
 	@Then("Options should be available")
 	public void options_should_be_available(io.cucumber.datatable.DataTable dataTable) {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-		// Double, Byte, Short, Long, BigInteger or BigDecimal.
-		//
-		// For other transformations you can register a DataTableType.
-		throw new io.cucumber.java.PendingException();
+		
 	}
 
 	@Then("options count should be {int}")
