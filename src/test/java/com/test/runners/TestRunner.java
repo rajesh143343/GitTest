89971bc3ab34953fc24bb31e84ccba8be9a32380
Rajesh <rajesh.vadlamudi999@gmail.com>
2021-05-19 19:39:58 +0530

    Added reports

--- src/test/java/com/test/runners/TestRunner.java
+++ src/test/java/com/test/runners/TestRunner.java
@@ -6,17 +6,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
- features = { "F:\\eclipse-workspace\\SHOPPINGTEST\\src\\test\\resources\\com\\shopping\\features"}
- ,glue={"com.shopping.stepdefinitions", "appHooks"}
- ,plugin= {"pretty",
- 			"json:target/MyReports/report.json",
- 			"junit:target/MyReports/report.xml"},
-		 publish = true,
-		 monochrome= true  
- )
+@CucumberOptions(features = {
+		"F:\\eclipse-workspace\\SHOPPINGTEST\\src\\test\\resources\\com\\shopping\\features" },
+			glue = {"com.shopping.stepdefinitions", "appHooks" }
+			,plugin= {"pretty",
+			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},// this is the extend plugin that we need for extend report refer pom.xml that plug is confi
+			publish = true, 
+			monochrome = true)
 
 public class TestRunner {
-	
 
 }
