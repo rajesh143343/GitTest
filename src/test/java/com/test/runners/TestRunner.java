93b7e6d9ca17613bd7cdbf82817456ed8ce9ecc2
Rajesh <rajesh.vadlamudi999@gmail.com>
2021-05-19 19:47:22 +0530

    updated runner class with reports plugin

--- src/test/java/com/test/runners/TestRunner.java
+++ src/test/java/com/test/runners/TestRunner.java
@@ -10,6 +10,9 @@ import io.cucumber.junit.CucumberOptions;
 		"F:\\eclipse-workspace\\SHOPPINGTEST\\src\\test\\resources\\com\\shopping\\features" },
 			glue = {"com.shopping.stepdefinitions", "appHooks" }
 			,plugin= {"pretty",
+					"json:target/MyReports/report.json",
+					"junit:target/MyReports/report.xml",
+		 			"html:target/cucumber-html-report",
 			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},// this is the extend plugin that we need for extend report refer pom.xml that plug is confi
 			publish = true, 
 			monochrome = true)
